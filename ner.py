2ab61abfd7d8e0937bacbb0ec0fc6d1d1cfc794a
Robin Camille Davis <rdvs18@gmail.com>
2016-01-09 16:30:25 -0500

    Input: URL; output: top 5 names

--- ner.py
+++ ner.py
@@ -1,36 +1,66 @@
 # Robin Camille Davis
 # CODEX 2016
 
-import nltk
+# Top 5 names from a .txt file
+
+##This script asks for the URL to a plain-text file (Project Gutenberg).
+##It outputs the top 5 person names from the text using the
+##Stanford Named Entity Recognition interface in NLTK.
+##
+##Caveats: first/last names and honorifics not considered
+
+import urllib2
+from nltk import tree
 from nltk import word_tokenize as tok
 from nltk import pos_tag as postag
 from nltk import ne_chunk as ne
 from nltk.corpus import gutenberg as gb
 from collections import Counter
 
-#print(gb.fileids())
-par = gb.raw("edgeworth-parents.txt")
-parshort = par[:10000]
+theurl = raw_input("URL to .txt file: ")
+sourcefile = urllib2.urlopen(theurl)
+source = sourcefile.read()
+
 
 #Tokenize
-partok = tok(parshort)
+sourcetok = tok(source)
 
 #Tag POS
-partag = postag(partok)
+sourcetag = postag(sourcetok)
 
 #Outputs POS-tagged text
-parne = ne(partag, binary=False)
+sourcene = ne(sourcetag, binary=False)
 
-chars = []
-for n in parne:
-    if type(n) == nltk.tree.Tree:
+charsall = []
+for n in sourcene:
+    if type(n) == tree.Tree:
         if n.label() == 'PERSON':
             for m in n:
-                chars.append(m[0])
+                charsall.append(m[0])
 
-words_to_count = (word for word in chars if word[:1].isupper())
-c = Counter(words_to_count)
-print c
+honorifics = ['Mr.', 'Mrs.', 'Ms.', 'Miss', 'Dr.', 'Prof.', 'Professor', 'Lord', 'Lady', 'Sir', 'Madam', 'Dame', 'Rev.', 'Rabbi']
 
-##Output:
-#Counter({u'Mary': 26, u'Peggy': 4, u'Mr.': 4, u'Annie': 3, u'Hopkins': 3, u'Edmund': 2, u'Nancy': 2, u'Isabella': 1, u'Harvey': 1, u'Navan': 1, u'Miss': 1, u'Maud': 1, u'Edgeworth': 1, u'Maria': 1})
+charsallnames = []
+for s in charsall:
+    if s in honorifics:
+        pass
+    else:
+        charsallnames.append(s)
+
+counted = (word for word in charsallnames if word[:1].isupper())
+c = Counter(counted)
+charscommon = c.most_common(5)
+
+chars = []
+for s in charscommon:
+    chars.append(s[0])
+
+print '\nMost common names:'
+print '\t'.join(chars)
+
+
+
+
+
+
+
